5042e95da13545d3b95b0e7a0a4fa58c7912253b
Mike <29170001+mike-prentice@users.noreply.github.com>
2022-04-23 18:08:56 -0400

    need to complete xml

--- src/main/java/services/DriverService.java
+++ src/main/java/services/DriverService.java
@@ -1,6 +1,11 @@
 package services;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 public class DriverService {
+    EntityManagerFactory emf = Persistence.createEntityManagerFactory(drivers)
 
     public Object findById() {
         return null;
